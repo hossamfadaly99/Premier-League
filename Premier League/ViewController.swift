3c40a15ebc4b2775d80d453ffe92d2048905d616
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-10-10 21:46:54 +0300

    Initial Commit

---
+++ Premier League/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Premier League
+//
+//  Created by Hossam on 10/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Premier LeagueUITests/Premier_LeagueUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Premier_LeagueUITestsLaunchTests.swift
+//  Premier LeagueUITests
+//
+//  Created by Hossam on 10/10/2023.
+//
+
+import XCTest
+
+final class Premier_LeagueUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
