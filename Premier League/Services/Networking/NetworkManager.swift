3fba824b45140a685861cf14fae5343b30f661ae
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-10-11 00:10:21 +0300

    separate constants

--- Premier League/Services/Networking/NetworkManager.swift
+++ Premier League/Services/Networking/NetworkManager.swift
@@ -10,8 +10,9 @@ import Alamofire
 
 class NetworkManager: Networkable {
   func fetchData<T>(url: String, param: [String : Any]?, completionHandler: @escaping (T?) -> Void) where T : Codable {
-    let header = HTTPHeader(name: "X-Auth-Token", value: "6233929f56b046dbb847b85f7fedbbc8")
+    let header = HTTPHeader(name: Constants.API_AUTH_KEY, value: Constants.API_AUTH_VALUE)
     AF.request(url, method: .get, parameters: param, headers: HTTPHeaders(arrayLiteral: header)).responseDecodable(of: T.self) { response in
+
       switch response.result {
       case .success(let result):
         completionHandler(result)
@@ -20,9 +21,6 @@ class NetworkManager: Networkable {
         print(error.localizedDescription)
         completionHandler(nil)
       }
-
     }
   }
-
-  
 }
---
+++ Premier League/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Premier League
+//
+//  Created by Hossam on 11/10/2023.
+//
+
+import Foundation
+
+class Constants {
+  static let API_AUTH_KEY = "X-Auth-Token"
+  static let API_AUTH_VALUE = "6233929f56b046dbb847b85f7fedbbc8"
+  static let BASE_URL = "https://api.football-data.org/v2/"
+  static let DATE_FROM = "dateFrom"
+  static let DATE_TO = "dateTo"
+
+}
