b6b5f207e37479e8bbe3acafed8f476b1063d924
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-10-11 12:18:40 +0300

    initialize home ui
    - setup matches list
    - setup match cell ui

--- Premier League/Modules/Matches/View/MatchesViewController.swift
+++ Premier League/Modules/Matches/View/MatchesViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class MatchesViewController: UIViewController {
 
+  @IBOutlet weak var matchesTableView: UITableView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    setupTableView()
+  }
+
+  private func setupTableView() {
+    let matchCell = UINib(nibName: "MatchCell", bundle: nil)
+    self.matchesTableView.register(matchCell, forCellReuseIdentifier: "MatchCell")
+    self.matchesTableView.dataSource = self
+  }
+}
+
+extension MatchesViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    3
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let matchCell = tableView.dequeueReusableCell(withIdentifier: "MatchCell") as! MatchCell
+
+    return matchCell
   }
 
 
 }
-
